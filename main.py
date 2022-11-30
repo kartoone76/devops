389cfae5c5d620afd7af26728f4a34cf17ae2744
Brian Toone <115804135+kartoone76@users.noreply.github.com>
2022-11-30 18:22:39 +0000

    added more indentation to make flake8 happy

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 ''' example module with a few math functions '''
 def square(n):
-  ''' returns the square of the parameter '''
-  return n*n
+    ''' returns the square of the parameter '''
+    return n*n
 
 def cube(n):
-  return n*n*n
+    return n*n*n
 
